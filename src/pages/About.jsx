6fe81b09b5bc891c7859ff106cb1c21804147b6b
Jinyang-park <dbsskdud60@gmail.com>
2023-04-30 23:15:44 +0900

    [doc] text animation working on

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 
 function About() {
-  return <div>About</div>;
+  const [position, setPosition] = useState(0);
+  function onScroll() {
+    setPosition(window.scrollY);
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <AboutBackground>
+        <Left>
+          <ScrollLeftText style={{ transform: `translateX(${-position}px)` }}>
+            Welcome Welcome Welcome
+          </ScrollLeftText>
+        </Left>
+        <Right>
+          <ScrollrightText style={{ transform: `translateX(${position}px)` }}>
+            Welcome Welcome Welcome
+          </ScrollrightText>
+        </Right>
+      </AboutBackground>
+    </>
+  );
 }
 
 export default About;
+export const AboutBackground = styled.div`
+  /* width: 100%; */
+  overflow: hidden;
+  height: 100vh;
+  /* overflow: hidden; */
+  background-color: #af9fff;
+`;
+export const Left = styled.div`
+  /* position: relative; */
+  /* width: 1050vw; */
+  top: 100%;
+  /* left: -800px;
+  white-space: nowrap; */
+`;
+export const ScrollLeftText = styled.div`
+  font-size: 100px;
+`;
+export const Right = styled.div`
+  /* position: relative; */
+  /* width: 1050vw; */
+  top: 100%;
+  /* right: -800px;
+  white-space: nowrap; */
+`;
+export const ScrollrightText = styled.div`
+  font-size: 100px;
+`;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,27 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
+import About from './About';
 
 function Main() {
   return (
-    <Introduce>
-      <MinimeHoduWrap>
-        <MinimeHouduImg />
-      </MinimeHoduWrap>
-      <IntroContent>
-        <IntronameWrap>
-          <Introname>Hi. I'm Jinyang.</Introname>
-        </IntronameWrap>
-        <IntrojobWrap>
-          <Introjob>A Frontend Developer </Introjob>
-        </IntrojobWrap>
-        <IntrojobWrap2>
-          <Introjob2>& Designer.</Introjob2>
-        </IntrojobWrap2>
-      </IntroContent>
-      <MinimeWrap>
-        <MinimeImg />
-      </MinimeWrap>
-    </Introduce>
+    <>
+      <Introduce>
+        <MinimeHoduWrap>
+          <MinimeHouduImg />
+        </MinimeHoduWrap>
+        <TitleBallon>
+          <TitleTxtCol>
+            <TitleTxt>Hire Jinyang!</TitleTxt>
+          </TitleTxtCol>
+          <TitleTail>
+            <TitleSvg
+              width='14'
+              height='16'
+              viewBox='0 0 14 16'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+              data-v-31c4c148=''
+            >
+              <TitlePath
+                fill-rule='evenodd'
+                clip-rule='evenodd'
+                d='M0 0.516602C0 9.06769 3.26836 15.9997 13.7618 15.9997L14 16.0005C13.0623 15.2567 12.2689 14.3944 11.6314 13.4229C10.4112 11.5638 10 9.73924 10 5.406V0.516602H0Z'
+                fill='#A6814C'
+                data-v-31c4c148=''
+              ></TitlePath>
+            </TitleSvg>
+          </TitleTail>
+        </TitleBallon>
+        <IntroContent>
+          <IntronameWrap>
+            <Introname>Hi. I'm Jinyang.</Introname>
+          </IntronameWrap>
+          <IntrojobWrap>
+            <IntrojobBox>
+              <ProgressBar>
+                <Introjob>A Frontend </Introjob>
+              </ProgressBar>
+              <Introjob3>Developer </Introjob3>
+            </IntrojobBox>
+          </IntrojobWrap>
+          <IntrojobWrap2>
+            <Introjob2>& Designer.</Introjob2>
+          </IntrojobWrap2>
+        </IntroContent>
+        <MinimeWrap>
+          <MinimeImg />
+        </MinimeWrap>
+      </Introduce>
+      <About />
+    </>
   );
 }
 
@@ -29,6 +61,7 @@ export default Main;
 
 export const Introduce = styled.div`
   padding-top: 150px;
+  margin-bottom: 100px;
   /* display: flex; */
 `;
 export const IntroContent = styled.div`
@@ -37,8 +70,40 @@ export const IntroContent = styled.div`
   align-items: flex-start;
   padding-left: 30px;
 `;
+export const TitleBallon = styled.div`
+  position: absolute;
+  display: flex;
+  margin-top: -115px;
+  margin-left: 19%;
+`;
+export const TitleTxtCol = styled.span`
+  background-color: #a6814c;
+  /* font-size: 25px; */
+  line-height: 30px;
+  padding: 5px 10px;
+  border-radius: 8px;
+  text-align: left;
+  color: #f9f5ef;
+  white-space: nowrap;
+`;
+export const TitleTxt = styled.span`
+  position: relative;
+
+  display: block;
+  font-size: 14px;
+`;
+export const TitleTail = styled.i`
+  position: absolute;
+  width: 14px;
+  height: 17px;
+  transform: scaleX(-1);
+  bottom: -2px;
+  left: -4px;
+`;
+export const TitleSvg = styled.svg``;
+export const TitlePath = styled.path``;
 export const IntronameWrap = styled.div`
-  width: 60%;
+  width: 50%;
   justify-content: center;
   align-items: center;
   height: 150px;
@@ -54,17 +119,21 @@ export const Introname = styled.h1`
   justify-content: center;
 `;
 export const IntrojobWrap = styled.div`
-  width: 70%;
+  width: 68%;
   justify-content: center;
   align-items: center;
   height: 150px;
-  color: black;
-  background-color: #af9fff;
+  color: #af9fff;
+  background-color: black;
   border-radius: 0px 100px 100px 0px;
 `;
-
+export const IntrojobBox = styled.div`
+  display: flex;
+  /* margin-left: 0px 80px; */
+  /* padding: 0px 80px; */
+`;
 export const IntrojobWrap2 = styled.div`
-  width: 45%;
+  width: 40%;
   justify-content: center;
   align-items: center;
   height: 150px;
@@ -73,13 +142,31 @@ export const IntrojobWrap2 = styled.div`
   background-color: black;
   padding: 0px 20px;
 `;
-export const Introjob = styled.p`
+export const Introjob = styled.span`
   font-size: 100px;
   font-family: 'Darker Grotesque', sans-serif;
   justify-content: center;
   display: flex;
 `;
-export const Introjob2 = styled.p`
+export const Introjob3 = styled.span`
+  font-size: 100px;
+  font-family: 'Darker Grotesque', sans-serif;
+  justify-content: center;
+  margin-left: 30px;
+`;
+export const ProgressBar = styled.div`
+  /* width: 150%; */
+  /* justify-content: center;
+  align-items: center; */
+  height: 150px;
+  color: black;
+  border-radius: 0px 100px 100px 0px;
+  background-color: #af9fff;
+  border-radius: 0px 100px 100px 0px;
+  padding: 0px 80px;
+`;
+
+export const Introjob2 = styled.span`
   font-size: 100px;
   font-family: 'Darker Grotesque', sans-serif;
   display: flex;
@@ -87,7 +174,7 @@ export const Introjob2 = styled.p`
 `;
 export const MinimeWrap = styled.div`
   position: absolute;
-  top: 270px;
+  top: 400px;
   left: 70%;
   /* padding-top: 50px; */
 `;
@@ -130,65 +217,64 @@ export const MinimeImg = styled.figure`
   }
   /* width: 375px;
   height: 405px; */
-  width: 245px;
+  width: 400px;
   height: 300px;
   /* background-image: url('/assets/jin.png'); */
   background-image: url('/assets/82.png');
-  margin: 0 auto;
+
   -webkit-animation: jin 2s steps(8) infinite;
   -moz-animation: jin 2s steps(8) infinite;
   -o-animation: jin 2s steps(8) infinite;
   animation: jin 2s steps(8) infinite;
 `;
 export const MinimeHoduWrap = styled.div`
-  width: 10%;
-  height: 10%;
-  /* width: 120rem;
-  height: 90rem; */
+  width: 30%;
 `;
 export const MinimeHouduImg = styled.figure`
-  @-webkit-keyframes hodu4 {
+  @-webkit-keyframes hodu7 {
     from {
       background-position: 0px;
     }
     to {
-      background-position: -3750px;
+      background-position: -2358px;
     } /* <-- width of spritesheet*/
   }
 
-  @-moz-keyframes hodu4 {
+  @-moz-keyframes hodu7 {
     from {
       background-position: 0px;
     }
     to {
-      background-position: -3750px;
+      background-position: -2358px;
     } /* <-- width of spritesheet*/
   }
 
-  @-o-keyframes hodu4 {
+  @-o-keyframes hodu7 {
     from {
       background-position: 0px;
     }
     to {
-      background-position: -3750px;
+      background-position: -2358px;
     } /* <-- width of spritesheet*/
   }
 
-  @keyframes hodu4 {
+  @keyframes hodu7 {
     from {
       background-position: 0px;
     }
     to {
-      background-position: -3750px;
+      background-position: -2358px;
     } /* <-- width of spritesheet*/
   }
-  width: 150px;
-  height: 150px;
+  width: 131px;
+  height: 100px;
   /* width: 50%;
   height: 50%; */
-  background-image: url('/assets/hodu4.png');
-  -webkit-animation: hodu4 2s steps(20) infinite;
-  -moz-animation: hodu4 2s steps(20) infinite;
-  -o-animation: hodu4 2s steps(20) infinite;
-  animation: hodu4 2s steps(20) infinite;
+  /* background-image: url('/assets/hodu4.png'); */
+  margin: 0 auto;
+  background-image: url('/assets/hodu7.png');
+  -webkit-animation: hodu7 2s steps(18) infinite;
+  -moz-animation: hodu7 2s steps(18) infinite;
+  -o-animation: hodu7 2s steps(18) infinite;
+  animation: hodu7 2s steps(18) infinite;
 `;
