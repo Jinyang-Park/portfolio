370b4b3dfeb2992a5f5c6146b405ba555d1d9b4b
Jinyang-park <dbsskdud60@gmail.com>
2023-05-15 15:03:34 +0900

    [doc] 모바일 반응형 상단 스크롤 없앰

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -7,8 +7,10 @@ import Contact from '../pages/Contact';
 import Header from '../pages/Header';
 import ScrollUpto from '../pages/ScrollUpto';
 import ScrollToTopButton from '../pages/ScrollToButton';
-
+import { useMediaQuery } from 'react-responsive';
 const Router = () => {
+  const isMobile = useMediaQuery({ query: '(max-width:768px)' });
+
   return (
     <BrowserRouter>
       <ScrollUpto />
@@ -19,7 +21,7 @@ const Router = () => {
         <Route path='/Projects' element={<Projects />}></Route>
         <Route path='/Contact' element={<Contact />}></Route>
       </Routes>
-      <ScrollToTopButton />
+      {isMobile ? <></> : <ScrollToTopButton />}
     </BrowserRouter>
   );
 };
